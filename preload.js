e8c671021bfc73d86523803efe882358b9b5aae1
Fritz Jay <jonathan.jay10@gmail.com>
2019-06-26 13:11:58 -0700

    updated preload.js to test functionality

--- preload.js
+++ preload.js
@@ -1,7 +1,27 @@
+const {
+  updatePDF,
+  FIELDS
+} = require('./pdf')
+
+// Temp vars
+const INPUT_PATH = './pdf/input.pdf'
+const OUTPUT_PATH = './output/output.pdf'
+
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
   for (const versionType of ['chrome', 'electron', 'node']) {
     document.getElementById(`${versionType}-version`).innerText = process.versions[versionType]
   }
+
+  const fields = {
+    [FIELDS.dateDay]: '05',
+    [FIELDS.dateMonth]: '19',
+    [FIELDS.dateYear]: '1991',
+    [FIELDS.technician]: 'Test',
+    [FIELDS.so]: 'Test',
+    [FIELDS.customer]: 'Test'
+  }
+
+  updatePDF(INPUT_PATH, OUTPUT_PATH, fields)
 })

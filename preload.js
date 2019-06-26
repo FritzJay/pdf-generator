f5f089e4ab95923971176b12f4d128c0f66387a4
Fritz Jay <jonathan.jay10@gmail.com>
2019-06-26 13:54:07 -0700

    Added form handling

--- preload.js
+++ preload.js
@@ -4,24 +4,43 @@ const {
 } = require('./pdf')
 
 // Temp vars
-const INPUT_PATH = './pdf/input.pdf'
+const INPUT_PATH = './input/input.pdf'
 const OUTPUT_PATH = './output/output.pdf'
 
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
-  for (const versionType of ['chrome', 'electron', 'node']) {
-    document.getElementById(`${versionType}-version`).innerText = process.versions[versionType]
-  }
-
-  const fields = {
-    [FIELDS.dateDay]: '05',
-    [FIELDS.dateMonth]: '19',
-    [FIELDS.dateYear]: '1991',
-    [FIELDS.technician]: 'Test',
-    [FIELDS.so]: 'Test',
-    [FIELDS.customer]: 'Test'
-  }
-
-  updatePDF(INPUT_PATH, OUTPUT_PATH, fields)
+  _initializeForms()
 })
+
+function submitForm() {
+  const inputs = document.querySelectorAll('#default-form input')
+  const data = _formatInputsData(inputs)
+  updatePDF(INPUT_PATH, OUTPUT_PATH, data)
+}
+
+function _formatInputsData(inputs) {
+  const fields = {}
+  inputs.forEach(function (input) {
+    if (input.name === 'Date') {
+      [year, month, day] = input.value.split('-')
+      fields[FIELDS.dateDay] = day
+      fields[FIELDS.dateMonth] = month
+      fields[FIELDS.dateYear] = year
+    } else {
+      fields[input.name] = input.value
+    }
+  })
+  return fields
+}
+
+function _getDateValues(date) {
+  return
+}
+
+function _initializeForms() {
+  document.getElementById('default-form').onsubmit = function (event) {
+    event.preventDefault()
+    submitForm()
+  }
+}
